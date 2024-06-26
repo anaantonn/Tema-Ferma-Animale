f2732e9221267c2dc1b1120a6a80e424a207f65e
Ana-Maria ANTON <anuta96@yahoo.com>
2024-06-26 14:53:13 +0300

    Definitii functii

--- Functions.h
+++ Functions.h
@@ -1,15 +1,9 @@
 #pragma once
 #include <iostream>
-#include <vector>
-#include <string>
 #include "Animal.h"
 
 using namespace std;
 
-ostream& operator << (ostream& os, const Animal& a);
-istream& operator >> (istream& in, Animal& a);
-string toLowerCase(const string& str);
-vector<Animal>::iterator findAnimalByName(vector<Animal>& animals, const string& name);
 int showMenu();
 void addNewAnimal(vector<Animal>& animals);
 void showEntireList(vector<Animal>& animals);
